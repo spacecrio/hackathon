ee2919f1215faf9970a2d5cb77751e67e0877276
Roman Kurakin <crio@fastmail.com>
2018-10-12 16:23:40 +0700

    Remove gulp

--- src/controlles/user.js
+++ src/controlles/user.js
@@ -1,17 +1,16 @@
 const userModel = require("../models/user");
 const jwt = require("../utlis/jwt");
-const ah = require("../middleware/asyncHandler");
 
-const register = ah((req, res) => {
+async function register(req, res) {
   const data = { username: req.body.username, password: req.body.password };
 
-  return userModel
-    .create(data)
-    .then(user => {
-      const token = jwt.issue({ id: user.id });
-      return res.status(200).json({ token, user });
-    })
-    .catch(() => res.status(409).json({ error: "username already taken" }));
-});
+  try {
+    const user = await userModel.create(data);
+    const token = jwt.issue({ id: user.id });
+    return res.status(200).json({ token, user });
+  } catch (e) {
+    return res.status(409).json({ error: "username already taken" });
+  }
+}
 
 module.exports = { register };
